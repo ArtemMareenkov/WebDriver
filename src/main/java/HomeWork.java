eff1aeb51e62998314cc17f3342757f3c79ec03c
ArtemMareenkov <artem.mareenkov@gmail.com>
2016-11-27 13:38:24 +0200

    Add verifications left

--- src/main/java/HomeWork.java
+++ src/main/java/HomeWork.java
@@ -24,10 +24,11 @@ public class HomeWork {
         passwordInput.sendKeys("123");// password enter
 
 
-        WebElement LoginButton = driver.findElement(By.id("logIn")); // Loginbutton Fiend
+        WebElement LoginButton = driver.findElement(By.id("logIn")); // Login button Fiend
         LoginButton.click(); //Login button click
 
-        driver.findElement(By.id("")).click();
+        WebElement insertbutton = driver.findElement(By.id("Will be added")); // insert player button fiend
+        insertbutton.click();// insert player button click
 
         String u = RandomStringUtils.random(12, true, true); // ramdom user name generate
         WebElement username = driver.findElement(By.id("ff14642ac1c__us_login"));
@@ -42,33 +43,65 @@ public class HomeWork {
         userpasswordconfirm.sendKeys(p);
 
         WebElement useremail = driver.findElement(By.id("ff14642ac1c__us_email"));
-        useremail.sendKeys(RandomStringUtils.random(10, true, true)"@gmail.com");
+        useremail.sendKeys(RandomStringUtils.random(10, true, true)+"@gmail.com");
 
         WebElement FirstName = driver.findElement(By.id("ff14642ac1c__us_fname"));
-        FirstName.sendKeys((RandomStringUtils.random(10, true, false));
+        FirstName.sendKeys(RandomStringUtils.random(10, true, false));
 
         WebElement LastName = driver.findElement(By.id("ff14642ac1c__us_lname"));
-        LastName.sendKeys((RandomStringUtils.random(10, true, false);
+        LastName.sendKeys(RandomStringUtils.random(10, true, false));
 
         WebElement City = driver.findElement(By.id("ff14642ac1c__us_city"));
-        City.sendKeys((RandomStringUtils.random(10, true, false);
+        City.sendKeys(RandomStringUtils.random(10, true, false));
 
-        WebElement Address = driver.findElement(By.id(""));
-        Address.sendKeys((RandomStringUtils.random(10, true, true));
+        WebElement Address = driver.findElement(By.id("ff14642ac1c__us_address"));
+        Address.sendKeys(RandomStringUtils.random(10, true, true));
 
-        WebElement PhoneNumber = driver.findElement(By.id(""));
-        PhoneNumber.sendKeys((RandomStringUtils.random(10, false, true));
+        WebElement PhoneNumber = driver.findElement(By.id("ff14642ac1c__us_phone"));
+        PhoneNumber.sendKeys(RandomStringUtils.random(10, false, true));
 
-        driver.findElement(By.name("button_save")); //save button click
+        WebElement SaveButton = driver.findElement(By.name("button_save"));
+         SaveButton.click(); //save button click
 
         String ExpectedResult = "Players";
         String ActualResult = driver.getTitle();
         assertString(ActualResult, ExpectedResult);
 
-        WebElement usersearch = driver.findElement(By.id(""));
+        WebElement usersearch = driver.findElement(By.id("It will be added later"));
         usersearch.sendKeys(u);
 
+        WebElement SearchButton = driver.findElement(By.id("It will be added later"));
+        SearchButton.click();
 
+        WebElement EditButton = driver.findElement(By.id("It will be added later"));
+        EditButton.click();
+
+        String FN = RandomStringUtils.random(12,true, false);
+        FirstName.sendKeys(FN);
+
+        String LN = RandomStringUtils.random(12, true,false );
+        LastName.sendKeys(LN);
+
+        String UsM = RandomStringUtils.random(12,true,true);
+        useremail.sendKeys(UsM);
+
+        String CT = RandomStringUtils.random(12,true,false);
+        City.sendKeys(CT);
+
+        String AD = RandomStringUtils.random(12,true,true);
+        Address.sendKeys(AD);
+
+        String PH = RandomStringUtils.random(11,false,true);
+        PhoneNumber.sendKeys(PH);
+
+        SaveButton.click();
+
+        usersearch.sendKeys(u);
+        EditButton.click();
+
+        String ExpectedResult = FN;
+        String ActualResult = FirstName.getAttribute("value");
+        assertString(ActualResult, ExpectedResult);
 
         driver.quit();
 
